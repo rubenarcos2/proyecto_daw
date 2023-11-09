5c8e7c02c0e8ffa986652ceda88daa1723ced70b
Rubén Arcos <rubenarcos2@gmail.com>
2023-11-09 13:07:00 +0100

    Frontend update v15
    Update frontend angular from v14 to v15

--- frontend/gesmerca/src/app/guards/auth.guard.ts
+++ frontend/gesmerca/src/app/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
   constructor(
     private authService: AuthService,
     private router: Router,
